d29cbc1e1c0a1aeb6e39f72ed69e80c9330c752e
Fatec Votorantim <159955708+fatec-votorantim@users.noreply.github.com>
2025-05-20 11:04:23 -0300

    Update usuarios.js

--- api/controllers/usuarios.js
+++ api/controllers/usuarios.js
@@ -3,18 +3,24 @@ import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
 export const insereUsuario = async (req, res) => {
-    req.body.avatar = `https://ui-avatars.com/api/?name=${req.body.nome.replace(/ /g, '+')}&background=F00&color=FFF`
-    //criptografia da senha
-    const salt = await bcrypt.genSalt(10) //10 rodadas de processamento do hash
-    req.body.senha = await bcrypt.hash(req.body.senha, salt)
-    //salvando o usuário...
-    const db = req.app.locals.db
-    await db.collection('usuarios')
-        .insertOne(req.body)
-        .then(result => res.status(201).send(result)
-            .catch(err => res.status(400).json(err))
-        )
+    try {
+        req.body.avatar = `https://ui-avatars.com/api/?name=${req.body.nome.replace(/ /g, '+')}&background=F00&color=FFF`
+        
+        // Criptografia da senha
+        const salt = await bcrypt.genSalt(10)
+        req.body.senha = await bcrypt.hash(req.body.senha, salt)
+
+        // Inserção no banco de dados
+        const db = req.app.locals.db
+        const result = await db.collection('usuarios').insertOne(req.body)
+
+        res.status(201).send(result)
+    } catch (err) {
+        console.error("Erro ao inserir usuário:", err)
+        res.status(400).json({ erro: err.message || "Erro desconhecido" })
+    }
 }
+
 
 export const efetuaLogin = async (req, res) => {
     const { email, senha} = req.body

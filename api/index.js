995dd851b44fadd4baffe93521aec564071aa79d
Ricardo Leme <ricardo.leme@gmail.com>
2025-06-10 09:07:59 -0300

    novo swagger

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { config } from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 import swaggerUI from 'swagger-ui-express'
 import cors from 'cors'
 import { connectToDatabase } from './config/db.js'
 import municipiosRoutes from './routes/municipios.js'
 import usuariosRoutes from './routes/usuarios.js'
-import swaggerDocument from './swagger/swagger_output.json' assert { type: "json" }
 
 config()
 const app = express()
@@ -14,22 +15,24 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-// Rota pública para arquivos estáticos
+// Rotas do sistema
 app.use('/', express.static('public'))
-
-// Rotas principais da API
 app.use('/api/municipios', municipiosRoutes)
 app.use('/api/usuarios', usuariosRoutes)
 
-// Rota de documentação do Swagger (sem custom CSS)
+// Carregamento do Swagger JSON de forma síncrona
+const swaggerFilePath = path.resolve('api/swagger/swagger_output.json')
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf8'))
+
+// Rota da documentação 
 app.use('/api/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // Favicon
 app.use('/favicon.ico', express.static('public/images/logo.png'))
 
-// Inicialização com conexão ao banco
+// Início do servidor
 connectToDatabase(app).then(() => {
     app.listen(PORT, () => {
-        console.log(`Servidor rodando na porta ${PORT}`)
+        console.log(`🚀Servidor rodando na porta ${PORT}`)
     })
 })

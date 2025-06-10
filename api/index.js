e0a63aa7debd7fafbcb6924d3bd20bb8ddec180b
Ricardo Leme <ricardo.leme@gmail.com>
2025-06-10 09:29:35 -0300

    volta o CSS

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ import usuariosRoutes from './routes/usuarios.js'
 config()
 const app = express()
 const PORT = process.env.PORT || 3000
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css" //CSS do Swagger para evitar erro no Vercel
 
 app.use(cors())
 app.use(express.json())
@@ -25,7 +26,7 @@ const swaggerFilePath = path.resolve('api/swagger/swagger_output.json')
 const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf8'))
 
 // Rota da documentação 
-app.use('/api/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use('/api/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument, { customCssUrl: CSS_URL }))
 
 // Favicon
 app.use('/favicon.ico', express.static('public/images/logo.png'))
